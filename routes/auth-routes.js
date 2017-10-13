2f9cf784a187dde33dd42dbdeb2bf82e7580930a
ashu-S <Ashwini0185@gmail.com>
2017-10-13 14:59:27 -0500

    changed the app.post passport.authentication function call, getting error in passport.js saying dbUser.validPassword is not a function

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -6,30 +6,34 @@ var passport = require("../config/passport");
 
 module.exports = function(app) {
 
-  app.post('/api/login', function(req,res,next){
-  	console.log('route fires');
-  	console.log("user name: " + req.body.user_name);
-  	console.log("password: " + req.body.password);
-	  passport.authenticate(req.body.user_name,req.body.password,function(info,user,err){
-	  	console.log("in passport.auth");
-	  		if (err) { return next(err) }
-	  		if (!user) {
-                console.log('bad');
-                req.session.messages = [info.message];
-                return res.redirect('/login')
-            }
-			req.logIn(user, function (err) {
-                    console.log('good');
-                    if (err) { return next(err); }
-                    return res.redirect('/admin');
-               });
-          })
+	app.post('/api/login', passport.authenticate('local', {successRedirect: '/admin', failureRedirect: '/login'}), function(req, res){ 
+			console.log('route fires');
+		  	console.log("user name: " + req.body.user_name);
+		  	console.log("password: " + req.body.password);
+			res.json(res);
+	});
 
-});
+ // 	app.post('/api/login', function(req,res,next){
+	//   	console.log('route fires');
+	//   	console.log("user name: " + req.body.user_name);
+	//   	console.log("password: " + req.body.password);
 
-  app.get("/logout", function(req, res) {
-    req.session.destroy(function(err) {
-      res.redirect("/");
-    });
-  });
+	//   	passport.authenticate(req.body.user_name,req.body.password,function(info,user,err){
+	//   		console.log("in passport.auth");
+	//   		if (err) { return next(err) }
+	//   		if (!user) {
+ //                console.log('bad');
+ //                req.session.messages = [info.message];
+ //                return res.redirect('/login')
+ //            }
+	// 		console.log('good');
+ //            return res.redirect('/admin');
+ //        });
+	// });
+
+ 	app.get("/logout", function(req, res) {
+    	req.session.destroy(function(err) {
+    		res.redirect("/");
+    	});
+  	});
 };
