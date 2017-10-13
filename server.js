f7be3cdd0aee318fd3e36f96e99f4549d5daef30
ashu-S <Ashwini0185@gmail.com>
2017-10-12 21:51:54 -0500

    Working on Passport authentication, passport.authenticate() not firing

--- server.js
+++ server.js
@@ -42,12 +42,13 @@ app.set("view engine", "handlebars");
 // =============================================================
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+require("./routes/auth-routes.js")(app);
 
 
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
