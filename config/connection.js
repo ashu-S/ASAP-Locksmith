3f5530e1a34f820d8e52ace993782836ba9aa7b9
krititiawri <kriit.tiwari28@gmail.com>
2017-10-16 14:52:32 -0500

    added connection.js file, with JAWSDB_URL for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,19 @@
-// // first we need to require the sequelize package
-// var Sequelize = require('sequelize');
-//
-// var env;
-// if (process.env.JAWSDB_URL)
-//     env = "production";
-// else {
-//     env = 'development';
-// }
-//
-// var config = require('./config')[env];
-//
-// // now we will set up our connection variable (sequelize) based on whether use_env_variable exists in our config
-// var sequelize;
-// if (config.use_env_variable) {
-// 	// jawsDB production option
-// 	sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-// 	// local mysqlDB development option
-// 	sequelize = new Sequelize(config.database, config.username, config.password, {
-// 		host: config.host,
-// 		dialect: config.dialect
-// 	});
-// }
-//
-// // finally export the connection to the model
-// module.exports = sequelize;
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL)
+{
+	connection=mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+	connection=mysql.createConnection({
+		host:"localhost",
+		user:"root",
+		password:"Kartik@01",
+		database:"asap_locksmith_db"
+	})
+};
+
+connection.connect();
+module.exports = connection;
